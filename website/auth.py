b8e98f75c5ad99118e5f23a282179628ce21a513
Phunbie <adeneyeoluwafunbi@gmail.com>
2022-09-21 19:22:51 +0000

    create database

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 #from flask_login import login_required,  current_user
 #from .models import Note
 #from .import db
@@ -17,4 +17,24 @@ def logout():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    firstname = request.form.get('firstName')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    if len(email) < 4:
+      flash('Email must be greater than 3 characters.',category='error')
+    elif len(firstname) < 2:
+      flash('First name must be greater than 1 character.',category='error')
+    elif password1 != password2:
+      flash('Passwords do not match.',category='error')
+    elif len(password1) < 7:
+      flash('Password must be greater than 6 characters.',category='error')
+    else:
+      flash('Welcome '+firstname, category='success')
+    
+  
+    
+    
   return render_template("signin.html")
---
+++ website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Jobs(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  company = db.Column(db.String(50), unique=True)
+  job = db.Column(db.String(150))
+  location = db.Column(db.String(150))
+  details = db.Column(db.String(1000))
+  date = db.Column(db.DateTime(timezone=True), default = func.now())
+  employer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  
+class User(db.Model,UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(50), unique=True)
+  password = db.Column(db.String(150))
+  firstname = db.Column(db.String(150))
+  joblist = db.relationship('Jobs')
