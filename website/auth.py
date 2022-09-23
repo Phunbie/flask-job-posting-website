fb399d176b02c9d6021001c2811c1eae6a6401bd
Phunbie <adeneyeoluwafunbi@gmail.com>
2022-09-23 10:13:41 +0000

    Implement login logout function

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash
-#from flask_login import login_required,  current_user
-#from .models import Note
-#from .import db
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from .models import User
+from .import db
 #import json
 
 auth = Blueprint('auth', __name__)
@@ -9,11 +10,29 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+  if request.method== 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+      if check_password_hash(user.password, password):
+        flash('Logged in sucessfully!', category='success')
+        login_user(user, remember=True)
+        return redirect(url_for('views.dashboard'))
+      else:
+        flash('Incorrect password, try again.', category='error')
+    else:
+      flash('Email does not exist.', category='error')
+      
+    
   return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-  return render_template("home.html")
+  logout_user()
+  return redirect(url_for('views.home'))
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -22,8 +41,11 @@ def signup():
     firstname = request.form.get('firstName')
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
-
-    if len(email) < 4:
+    
+    user = User.query.filter_by(email=email).first()
+    if user:
+      flash('Email already exists.', category='error')
+    elif len(email) < 4:
       flash('Email must be greater than 3 characters.',category='error')
     elif len(firstname) < 2:
       flash('First name must be greater than 1 character.',category='error')
@@ -32,8 +54,12 @@ def signup():
     elif len(password1) < 7:
       flash('Password must be greater than 6 characters.',category='error')
     else:
+      new_user = User(email=email, firstname=firstname, password=generate_password_hash(password1, method='sha256'))
+      db.session.add(new_user)
+      db.session.commit()
       flash('Welcome '+firstname, category='success')
-    
+      login_user(user, remember=True)
+      return redirect(url_for('views.dashboard'))
   
     
     
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request
-#from flask_login import login_required,  current_user
-#from .models import Note
-#from .import db
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required,  current_user
+from .models import Jobs
+from .import db
 #import json
 
 views = Blueprint('views', __name__)
@@ -16,6 +16,25 @@ def moreJobs():
   return render_template("More-jobs.html")
 
 @views.route('/dashboard', methods=['GET', 'POST'])
+@login_required
 def dashboard():
-  data = request.form
+  if request.method == 'POST':
+    company = request.form.get('company')
+    job = request.form.get('job')
+    location = request.form.get('location')
+    details = request.form.get('details')
+    if len(company) < 3:
+      flash('Company name must be greater than 2 characters.',category='error')
+    elif len(job) < 3:
+      flash('Job position must be greater than 2 characters.',category='error')
+    elif len(location) < 3:
+      flash('Location must be greater than 2 characters.',category='error')
+    elif len(details) < 3:
+      flash('Location must be greater than 2 characters.',category='error')
+    else:
+      new_job = Jobs(company=company, job=job, location=location, details=details)
+      db.session.add(new_job)
+      db.session.commit()
+      flash('New job added!', category='success')
+      
   return render_template("dashboard.html")
